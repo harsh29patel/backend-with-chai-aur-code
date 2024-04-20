b458e1319c86aaddd97b11d279375a880f07c2c3
harsh29patel <harshpatel290703@gmail.com>
2024-04-20 11:02:47 +0530

    asynchandler return

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,7 +1,7 @@
 //************************ METHOD 1**********
 
 const asyncHandler= (requestHandler)=>{
-    (res,req,next)=>{
+ return   (res,req,next)=>{
         Promise.resolve(requestHandler(res,req,next)).catch((err)=>next(err))
     }
 }
